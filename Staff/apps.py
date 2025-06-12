10e9f9f6f34350cb213ca378fbeb1914ac1f97f6
voste126 <steveaustine126@gmail.com>
2025-06-12 23:08:30 +0300

    FEAT: default admin in application

--- Staff/apps.py
+++ Staff/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
 
 
 class StaffConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Staff'
+
+    def ready(self):
+        User = get_user_model()
+        try:
+            if not User.objects.filter(email='admin@paramount.co.ke').exists():
+                User.objects.create_superuser(
+                    email='admin@paramount.co.ke',
+                    password='Admin@123',
+                    otp='123456',
+                    is_verified=True
+                )
+        except IntegrityError:
+            pass
